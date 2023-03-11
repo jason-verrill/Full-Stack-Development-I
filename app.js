18166a684638216664dc640655d4a049dee9ed72
Jason Verrill <jason_verrill@hotmail.com>
2023-03-11 13:32:19 -0500

    Missed files to track. Deleting extra files.

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Get public objects from installed packages
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Get public objects from page routes
+const usersRouter = require('./app_server/routes/users');
+const indexRouter = require('./app_server/routes/index');
+const travelRouter = require('./app_server/routes/travel');
+const roomsRouter = require('./app_server/routes/rooms');
+const mealsRouter = require('./app_server/routes/meals');
+const newsRouter = require('./app_server/routes/news');
+const aboutRouter = require('./app_server/routes/about');
+const contactRouter = require('./app_server/routes/contact');
 
-var app = express();
+const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 
+// Call package functions for all URLs
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+// Map URL paths to route functions
+// Function will be called when a matching URL is entered
 app.use('/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/travel', travelRouter);
+app.use('/rooms', roomsRouter);
+app.use('/meals', mealsRouter);
+app.use('/news', newsRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
