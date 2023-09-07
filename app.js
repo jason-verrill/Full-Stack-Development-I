b705990e7b413f743ba12c8134292379dea3adee
Jason Verrill <82787633+jason-verrill@users.noreply.github.com>
2023-09-07 11:58:40 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+// Get public objects from installed packages
+require('dotenv').config();
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs')
+const cors = require('cors');
+const corsOptions = {
+	origin: 'http://localhost:4200',
+	allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization']
+}
+
+// Connect the database model
+require('./app_api/models/db');
+require('./app_api/config/passport');
+
+// Get page routes from app_server
+const usersRouter = require('./app_server/routes/users');
+const indexRouter = require('./app_server/routes/index');
+const travelRouter = require('./app_server/routes/travel');
+const roomsRouter = require('./app_server/routes/rooms');
+const mealsRouter = require('./app_server/routes/meals');
+const newsRouter = require('./app_server/routes/news');
+const aboutRouter = require('./app_server/routes/about');
+const contactRouter = require('./app_server/routes/contact');
+
+// Get page routes from app_api
+const apiRouter = require('./app_api/routes/index');
+
+// Create express instance
+const app = express();
+
+// View engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
+app.set('view engine', 'hbs');
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+
+// Call package functions for all URLs
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Map URL paths to route functions
+// Function will be called when a matching URL is entered
+// app_server
+app.use('/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/travel', travelRouter);
+app.use('/rooms', roomsRouter);
+app.use('/meals', mealsRouter);
+app.use('/news', newsRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
+
+// app_api
+app.use('/api', cors(corsOptions), apiRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
